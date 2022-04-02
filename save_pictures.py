33aa000aed8555b7410b30ed03e6b537f2ef1a59
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-04-02 11:09:27 +0300

    splited function

--- save_pictures.py
+++ save_pictures.py
@@ -3,10 +3,13 @@ import requests
 
 
 def save_pic(pic_url, pic_path, headers=None):
-    if headers is None:
-        headers = {}
     picture = requests.get(pic_url, headers=headers)
-    directory = os.path.dirname(pic_path)
-    os.makedirs(directory, exist_ok=True)
+    check_folder(pic_path)
     with open(pic_path, 'wb') as file:
         file.write(picture.content)
+
+
+def check_folder(pic_path):
+    directory = os.path.dirname(pic_path)
+    os.makedirs(directory, exist_ok=True)
+    
