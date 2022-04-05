4b5dfedb15246929601abab05295a96397dd82b6
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-04-05 10:40:40 +0300

    All function arguments are specified

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from urllib.parse import urlparse
 
 import requests
+from dotenv import dotenv_values
 
-from main import NASA_API_KEY
 from save_pictures import save_pic
 
 
@@ -13,10 +13,10 @@ def get_file_extension(link):
     return os.path.splitext(urlparse(link).path)[1]
 
 
-def fetch_nasa_apod():
+def fetch_nasa_apod(nasa_api_key):
     nasa_apod_link = f"https://api.nasa.gov/planetary/apod"
     count = int(random.randrange(30, 50))
-    payload = {"count": count, "api_key": NASA_API_KEY}
+    payload = {"count": count, "api_key": nasa_api_key}
     nasa_links = requests.get(nasa_apod_link, params=payload).json()
     nasa_links.raise_for_status
     for number, apod in enumerate(nasa_links):
@@ -26,9 +26,9 @@ def fetch_nasa_apod():
         save_pic(pic_url, pic_path)
 
 
-def fetch_nasa_epic():
+def fetch_nasa_epic(nasa_api_key):
     nasa_epic_link = f"https://api.nasa.gov/EPIC/api/natural/images"
-    payload = {"api_key": NASA_API_KEY}
+    payload = {"api_key": nasa_api_key}
     epic_pictures = requests.get(nasa_epic_link, params=payload).json()
     epic_pictures.raise_for_status
     for number, picture in enumerate(epic_pictures):
@@ -36,7 +36,7 @@ def fetch_nasa_epic():
         date = picture["date"]
         date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
         date = date.strftime("%Y/%m/%d")
-        payload = {"api_key": NASA_API_KEY}
+        payload = {"api_key": nasa_api_key}
         pic_url = f"https://api.nasa.gov/EPIC/archive/natural/{date}" \
                   f"/png/{pic_name}.png"
         pic_path = f"images/NASA_EPIC/{pic_name}.png"
@@ -44,8 +44,9 @@ def fetch_nasa_epic():
 
 
 if __name__ == '__main__':
+    nasa_api_key = dotenv_values(".env")["NASA_API_KEY"]
     try:
-        fetch_nasa_epic()
-        fetch_nasa_apod()
+        fetch_nasa_epic(nasa_api_key)
+        fetch_nasa_apod(nasa_api_key)
     except requests.exceptions.HTTPError as error:
         exit("Can't get data from NASA-EPIC server:\n{0}".format(error))
--- main.py
+++ main.py
@@ -5,21 +5,20 @@ import telegram
 from dotenv import dotenv_values
 
 
-def post_endlessly(bot):
+def post_endlessly(bot, chat_id, posting_period):
     while True:
         for root, dirs, files in os.walk("images"):
             for filename in files:
                 image_path = f"{root}/{filename}"
                 with open(image_path, "rb") as file:
                     photo = file.read()
-                bot.send_photo(chat_id=CHAT_ID, photo=photo)
-                sleep(float(POSTING_PERIOD))
+                bot.send_photo(chat_id=chat_id, photo=photo)
+                sleep(float(posting_period))
 
 
 if __name__ == '__main__':
-    NASA_API_KEY = dotenv_values(".env")["NASA_API_KEY"]
-    TG_TOKEN = dotenv_values(".env")["TG_TOKEN"]
-    CHAT_ID = dotenv_values(".env")["CHAT_ID"]
-    POSTING_PERIOD = dotenv_values(".env")["POSTING_PERIOD"]
-    bot = telegram.Bot(token=TG_TOKEN)
-    post_endlessly(bot)
+    tg_token = dotenv_values(".env")["TG_TOKEN"]
+    chat_id = dotenv_values(".env")["CHAT_ID"]
+    posting_period = dotenv_values(".env")["POSTING_PERIOD"]
+    bot = telegram.Bot(token=tg_token)
+    post_endlessly(bot, chat_id, posting_period)
